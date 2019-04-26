6894ccbbf620794f62766e7438a10773df8bb8da
Justin Shaw <easyworkemail@gmail.com>
2019-04-26 22:00:20 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,10 @@
 # code the #greeting method here!
+
+
+def greeting(name)
+  if name == 'Don'
+    puts "Hello Don. It's nice to meet you."
+  else
+    puts "Hello Sally. It's nice to meet you."
+  end
+end
