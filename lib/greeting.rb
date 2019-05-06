74d7cec71c609fe13c8f2e7324e2f53202fc9865
Andrei-Nicolae Trandafir <ant34@bath.ac.uk>
2019-05-06 13:42:35 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,9 @@
 # code the #greeting method here!
+
+puts "Hi there, gamer! What's your name?"
+
+name = gets
+
+def greeting( name = "Ultimate Gamer")
+  puts "Wow, #{name}, that's the most epic name"
+end
