232a65f2a9d5cd18b09c0b20d1218c38a490f8c9
Ben Francisco <bpfranci@math.tamu.edu>
2019-08-18 20:41:06 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,7 @@
 # code the #greeting method here!
+
+# greeting: takes in a string for a user name and outputs
+#           a fixed string including that name.
+def greeting(name)
+  puts("Hello #{name.strip}. It's nice to meet you.")
+end
